9f37a06954890d3f5051b733ed39c2ffb6376901
USpiri <urielspiridione135@gmail.com>
2023-10-11 00:27:56 -0300

    ✨ Feat: Add posts Content Collection

---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { z, defineCollection } from "astro:content";
+
+const mainShema = z.object({
+  title: z.string(),
+  pubDate: z.date(),
+  description: z.string(),
+  tags: z.array(z.string()),
+  image: z.string().optional(),
+  video: z.string().optional(),
+});
+
+const mainCollection = defineCollection({
+  type: "content", // v2.5.0 and later
+  schema: mainShema,
+});
+
+export const collections = {
+  posts: mainCollection,
+};
