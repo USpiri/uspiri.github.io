4b19fd71cb9a9faebdee5e374f1441e7ed3ae30f
USpiri <urielspiridione135@gmail.com>
2023-10-11 22:53:15 -0300

    📦 Build: Set up gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://USpiri.github.io',
+  base: '/',
   integrations: [tailwind()],
   markdown: {
     shikiConfig: {
