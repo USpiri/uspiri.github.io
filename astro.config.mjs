0efb506fb09181cf2c51af5563d10b347b4b47d3
USpiri <urielspiridione135@gmail.com>
2023-10-12 01:19:31 -0300

    ✨ Feat: Astro sitemap and typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://USpiri.github.io',
   base: '/',
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'material-theme-darker',
       langs: [],
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
