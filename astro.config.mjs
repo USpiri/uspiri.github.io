67e6fd1ed0a551bc791ce02f80f0d690637d857f
USpiri <urielspiridione135@gmail.com>
2023-10-16 16:28:46 -0300

    ✨ Feat: TreeFile implementation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://USpiri.github.io',
   base: '/',
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), mdx()],
   markdown: {
     shikiConfig: {
       theme: 'material-theme-darker',
