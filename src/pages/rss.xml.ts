75b873fc888dfb446986db92d5e81fe8ad31555b
USpiri <urielspiridione135@gmail.com>
2023-10-18 11:59:34 -0300

    🐛 Fix: Rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,11 +1,16 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context: { site: any }) {
+  const posts = (await getCollection("posts")).map((post) => ({
+    ...post.data,
+    link: post.slug,
+  }));
   return rss({
     title: "Uriel spiridione | Blog",
     description: "Personal blog and repository",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
+    items: posts,
     customData: `<language>es-ar</language>`,
   });
 }
