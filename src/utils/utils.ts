9732524f85e8735cfa5792169140acb2c62fd896
USpiri <urielspiridione135@gmail.com>
2023-10-24 23:03:36 -0300

    ✨ Feat: GetFolder and GetFolders utilities

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,11 @@
 export const capitalize = (text: string): string => {
   return text.charAt(0).toUpperCase() + text.slice(1);
 };
+
+export const getFolder = (slug: string): string => {
+  return slug.split("/")[0];
+};
+
+export const getFolders = (docs: any): string[] => {
+  return [...new Set(docs.map((doc: any) => getFolder(doc.slug)))] as string[];
+};
