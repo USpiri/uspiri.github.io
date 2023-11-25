18399b7681fa957e4fa04c502ec267d39bbecfbc
USpiri <urielspiridione135@gmail.com>
2023-11-25 00:43:49 -0300

    ✨ Feat: Pagination component

---
+++ src/utils/models/page.interface.ts
@@ -0,0 +1,14 @@
+export interface Page<T = any> {
+  data: T[];
+  start: number;
+  end: number;
+  total: number;
+  currentPage: number;
+  size: number;
+  lastPage: number;
+  url: {
+    current: string;
+    prev: string | undefined;
+    next: string | undefined;
+  };
+}
